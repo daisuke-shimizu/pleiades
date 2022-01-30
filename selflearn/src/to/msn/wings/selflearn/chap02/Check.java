77bfdec5510bcc608a016a837b3f7c0bc0408dbb
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2022-01-30 19:15:15 +0900

    done chap02

---
+++ selflearn/src/to/msn/wings/selflearn/chap02/Check.java
@@ -0,0 +1,19 @@
+package to.msn.wings.selflearn.chap02;
+
+public class Check {
+
+	public static void main(String[] args) {
+		var value = 10;
+		System.out.println("ようこそ、\nJavaの世界へ");
+		String str = null;
+		int[][] data = new int[5][4];
+		var list = new int[][] {
+			{2, 3, 5},
+			{1, 2},
+			{10, 11, 12, 13},
+		};
+		
+		System.out.println(list[0][0]);
+	}
+
+}
